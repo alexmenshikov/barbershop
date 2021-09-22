07dfc185f081ee2831b2fd94562d35d76f8b5c02
menshikov <menshchikov.aleksandr@ya.ru>
2021-09-22 16:16:18 +0700

    create block header, advantage, about

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,14 @@
 $(document).ready(() => {
-	$(".category").click((e) => {
-		let currentElement = $(e.target);
-		$(".products-container").hide();
-		let id = currentElement.data("id");
-		$("#" + id).show();
-
-		$(".category").removeClass("active");
-		currentElement.addClass("active");
-
-		$("#" + id + " .products").slick("refresh");
-		$("#" + id + " .products-nav").slick("refresh");
-	});
-
 	// === mobile menu ===
-	$("#burger").click(() => {
-		$("#header").toggleClass("menu-open");
+	$(".burger").click(() => {
+		$(".menu").addClass("menu__mobile");
 	});
 
-	$("#header #menu ul li").click(() => {
-		$("#header").removeClass("menu-open");
+	$(".close").click(() => {
+		$(".menu").removeClass("menu__mobile");
+	});
+
+	$(".menu__item__link").click(() => {
+		$(".menu").removeClass("menu__mobile");
 	});
 });
